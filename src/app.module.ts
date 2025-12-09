ceb1dec15a2faf28aeb13457b417692953012455
Carlos-ortiz23 <carlosor3000@gmail.com>
2025-12-09 10:29:36 -0500

    feat: add root AppModule

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { ClientsModule } from './modules/clients/clients.module';
+import { TechniciansModule } from './modules/technicians/technicians.module';
+import { TicketsModule } from './modules/tickets/tickets.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+      inject: [ConfigService],
+    }),
+    CommonModule,
+    AuthModule,
+    UsersModule,
+    CategoriesModule,
+    ClientsModule,
+    TechniciansModule,
+    TicketsModule,
+  ],
+})
+export class AppModule {}
