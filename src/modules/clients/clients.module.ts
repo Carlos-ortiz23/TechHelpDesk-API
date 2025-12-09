2c9cb472acf7aca07a953c84ff51250580ec917e
Carlos-ortiz23 <carlosor3000@gmail.com>
2025-12-09 10:34:54 -0500

    feat(clients): add ClientsModule

---
+++ src/modules/clients/clients.module.ts
@@ -0,0 +1,17 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsService } from './clients.service';
+import { ClientsController } from './clients.controller';
+import { Client } from './entities/client.entity';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Client]),
+    forwardRef(() => AuthModule),
+  ],
+  controllers: [ClientsController],
+  providers: [ClientsService],
+  exports: [ClientsService],
+})
+export class ClientsModule {}
